808a443cc4581f0177e147cef1d778f8ffc6b5c2
Juan Trapero <i42trcaj@uco.es>
2025-12-10 17:09:22 +0100

    Primera subida

---
+++ src/catalog/catalog.h
@@ -0,0 +1,38 @@
+#ifndef CATALOG_H
+#define CATALOG_H
+
+#include "../persons/persons.h"
+#include <vector>
+
+
+
+class CyclistCatalog{
+
+    private:
+            std::vector <Cyclist> cyclists_;
+    
+    public:
+            bool Load(const std::string& path);
+            std::vector <Cyclist> Data() {return cyclists_;}
+            std::size_t Size() {return Data().size();}
+            std::string GetTeam (std::string cyclist_id);
+            std::vector <Cyclist> GetByTeam(std::string team);
+            std::vector <Cyclist> GetYoungest();
+
+
+};
+
+
+class DirectorCatalog{
+
+        private:
+                std::vector <Director> directors_;
+        
+        public:
+                bool Load(const std::string& path);
+                std::vector <Director>& Data() {return directors_;}
+                std::size_t Size() {return Data().size();}
+
+};
+
+#endif
